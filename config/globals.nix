5e7647700e955247d4f9ff25fb56cf9f68fced48
literally-sai <173792483+literally-sai@users.noreply.github.com>
2025-05-09 00:27:33 +0200

    Update

--- config/globals.nix
+++ config/globals.nix
@@ -3,10 +3,5 @@
     mapleader = " ";
     maplocalleader = " ";
     have_nerd_font = true;
-
-    loaded_node_provider = 0;
-    loaded_python3_provider = 0;
-    loaded_perl_provider = 0;
-    loaded_ruby_provider = 0;
   };
 }
