cff99661d5d2518d352903265d20afd9ff5b4185
literally-sai <173792483+literally-sai@users.noreply.github.com>
2025-05-08 23:22:22 +0200

    Add more colors

--- config/themes/colors/cyberdream.nix
+++ config/themes/colors/cyberdream.nix
@@ -1,18 +1,18 @@
 {
-  base00 = "#16181a"; # Background
-  base01 = "#1e2022"; # Lighter background
-  base02 = "#282a2e"; # Selection background
-  base03 = "#3c3e44"; # Comments
-  base04 = "#5c5e66"; # Dark foreground
-  base05 = "#d8dee9"; # Default foreground
-  base06 = "#e6e6e6"; # Light foreground
-  base07 = "#f5f5f5"; # Light background
-  base08 = "#ff6e6e"; # Red (errors, etc.)
-  base09 = "#ffa066"; # Orange
-  base0A = "#ffcc66"; # Yellow
-  base0B = "#66ff66"; # Green
-  base0C = "#66cccc"; # Cyan
-  base0D = "#6699ff"; # Blue
-  base0E = "#cc66ff"; # Purple
-  base0F = "#ff99cc"; # Pink
+  base00 = "#16181a";
+  base01 = "#1e2022";
+  base02 = "#282a2e";
+  base03 = "#3c3e44";
+  base04 = "#5c5e66";
+  base05 = "#d8dee9";
+  base06 = "#e6e6e6";
+  base07 = "#f5f5f5";
+  base08 = "#ff6e6e";
+  base09 = "#ffa066";
+  base0A = "#ffcc66";
+  base0B = "#66ff66";
+  base0C = "#66cccc";
+  base0D = "#6699ff";
+  base0E = "#cc66ff";
+  base0F = "#ff99cc";
 }
